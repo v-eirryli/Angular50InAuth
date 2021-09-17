f86eb4e6b3b6fd01ce2e0454fb9873fcf6becc6d
Eirry Li <v-eirryli@microsoft.com>
2021-09-17 00:11:32 -0700

    Add project files.

---
+++ Angular50InAuth/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Angular50InAuth.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular50InAuth.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
